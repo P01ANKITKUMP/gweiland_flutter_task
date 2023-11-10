947df42aaf2b84e88f17e0257e595c35ae72dd8f
P01ANKITKUMP <Ankit.Panchal@vitalitygroup.com>
2023-11-10 16:24:37 +0530

    Revert "init commit"
    This reverts commit c1d1d2cf5de97e018d8f83f3762b6807ac569087.

--- android/app/src/main/java/com/example/gweiland_flutter_task/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.gweiland_flutter_task;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/utils/routes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:gweiland_flutter_task/utils/common_exports.dart';
-
-@immutable
-class AppRoutes {
-  const AppRoutes._();
-  static const String homeRoutes = '/homeRoutes';
-}
